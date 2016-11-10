3d303cd377bcf5ed3d04bdf387e863cf0819c3c0
felixdamians <damian.sanabria@hotmail.com>
2016-11-10 20:18:54 -0300

    TP3.2C.SanabriaFelixDamian

---
+++ Sanabria.FelixDamian2C.TP3-/Archivos/Texto.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Excepciones;
+
+namespace Archivos
+{
+    public class Texto: IArchivo<string>
+    {
+        /// <summary>
+        /// METODO QUE GUARDA UN OBJETO EN FORMATO STRING
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos">DATO A GUARDAR EN EL ARCHIVO </param>
+        /// <returns></returns>
+        public bool guardar(string archivo, string datos)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(archivo, true))
+                {
+                    sw.WriteLine(datos);
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                throw new ArchivosException(e);
+            }
+        }
+        /// <summary>
+        /// METODO PUBLICO QUE GUARDARA UN ARCHIVO DE TEXTO
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        public bool leer(string archivo, out string datos)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(archivo))
+                {
+                    datos = sr.ReadToEnd();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                datos = "";
+                throw new ArchivosException(e);
+            }
+
+        }
+    }
+}
---
+++ Sanabria.FelixDamian2C.TP3-/EntidadesAbstractas/PersonaGimnasio.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Excepciones;
+
+namespace EntidadesAbstractas
+{
+    public abstract class PersonaGimnasio : Persona
+    {
+        private int _identificador;
+
+        #region "CONSTRUCTORES"
+        /// <summary>
+        /// CONSTRUCTOR DE PERSONA GIMNASIO PARA SERIALIZAR 
+        /// </summary>
+        public PersonaGimnasio()
+        { 
+        
+        }
+        public PersonaGimnasio(int id, string nombre, string apellido, string dni, ENacionalidad nacionalidad)
+            : base(nombre, apellido, dni, nacionalidad)
+        {
+            this._identificador = id;
+        }
+        #endregion
+
+        #region "Metodos
+        /// <summary>
+        /// METODO PROTEGIDO VIRTUAL, EL CUAL MUESTRA LOS DATOS DE PERSONA 
+        /// </summary>
+        /// <returns></returns>
+        protected virtual string MostrarDatos()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat(base.ToString());
+            sb.AppendFormat("\nCARNET NUMERO: {0}", this._identificador);
+            sb.AppendLine("");
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// METODO ABSTRACTO QUE RETORNA UN STRING
+        /// </summary>
+        /// <returns></returns>
+        protected abstract string ParticiparEnClase();
+        /// <summary>
+        /// COMPARA DOS OBJETOS. 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object obj)
+        {
+            if (obj is PersonaGimnasio)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// VERIFICA SI DOS PERSONAS SON IGUALES POR SU ID O DNI (OBJETO PERSONA)
+        /// </summary>
+        /// <param name="pg1"> OBJETO PersonaGimnasio 1 a comparar</param>
+        /// <param name="pg2"> OBJETO PersonaGimnasio 2 a comparar</param>
+        /// <returns></returns>
+        public static bool operator ==(PersonaGimnasio pg1, PersonaGimnasio pg2)
+        {
+            if (!Object.ReferenceEquals(pg1, null) && !Object.ReferenceEquals(pg2, null))
+            {
+                if (pg1.Equals(pg2))
+                {
+                    if (pg1._identificador == pg2._identificador || pg1.DNI == pg2.DNI)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// VERIFICA SI DOS PERSONAS SON DISTINTAS POR SU ID O DNI (OBJETO PERSONA)
+        /// </summary>
+        /// <param name="pg1">PersonaGimnasio 1 a comparar</param>
+        /// <param name="pg2">PersonaGimnasio 2 a comparar</param>
+        /// <returns></returns>
+        public static bool operator !=(PersonaGimnasio pg1, PersonaGimnasio pg2)
+        {
+            return !(pg1 == pg2);
+        }
+
+        #endregion
+
+    }
+}
